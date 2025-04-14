44a6a9cf47df1648d930f5e0d409de5945ea909e
ubay1 <tomdelonge976@gmail.com>
2025-04-14 13:00:36 +0700

    fix component modal

--- src/utils/tw-variants/components/modal.ts
+++ src/utils/tw-variants/components/modal.ts
@@ -6,7 +6,7 @@ export const modalVariant = tv({
     wrapperContent: 'fixed inset-0 flex items-center justify-center z-50 py-8',
     cardContent:
       'relative bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 pt-4 w-[90%] md:w-1/2 max-h-[90vh] overflow-y-auto',
-    buttonX: 'absolute top-2.5 right-3 bg-transparent border-none text-2xl cursor-pointer',
+    buttonX: 'absolute top-1 right-3 bg-transparent border-none text-2xl cursor-pointer dark:text-white',
   },
   variants: {
     withBgBlur: {
